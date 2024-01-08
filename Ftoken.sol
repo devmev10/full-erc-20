3137a373a053cf88b2c5aeb927a247eb11e5085c
devmev10 <ravitokas90@gmail.com>
2024-01-08 15:37:04 +0530

    add pausable

--- Ftoken.sol
+++ Ftoken.sol
@@ -2,6 +2,7 @@
 pragma solidity ^0.8.20;
 import "@openzeppelin/contracts/token/ERC20/ERC20.sol";
 import "@openzeppelin/contracts/token/ERC20/extensions/ERC20Burnable.sol";
+import "@openzeppelin/contracts/token/ERC20/extensions/ERC20Pausable.sol";
 
 contract Ftoken is ERC20, ERC20Burnable {
     constructor() ERC20("FTOKEN", "FTK") {
