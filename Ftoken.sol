c5adc7aa2d0965b330aadf90799863492a841211
devmev10 <ravitokas90@gmail.com>
2024-01-08 15:38:05 +0530

    add ownable

--- Ftoken.sol
+++ Ftoken.sol
@@ -3,6 +3,7 @@ pragma solidity ^0.8.20;
 import "@openzeppelin/contracts/token/ERC20/ERC20.sol";
 import "@openzeppelin/contracts/token/ERC20/extensions/ERC20Burnable.sol";
 import "@openzeppelin/contracts/token/ERC20/extensions/ERC20Pausable.sol";
+import "@openzeppelin/contracts/access/Ownable.sol";
 
 contract Ftoken is ERC20, ERC20Burnable {
     constructor() ERC20("FTOKEN", "FTK") {
