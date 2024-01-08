e1808221be6e534dacca0f7b0d2c65a62600ee16
devmev10 <ravitokas90@gmail.com>
2024-01-08 15:05:28 +0530

    build(deps): update Ftoken.sol and package-lock.json

--- Ftoken.sol
+++ Ftoken.sol
@@ -3,7 +3,7 @@ pragma solidity ^0.8.20;
 import "@openzeppelin/contracts/token/ERC20/ERC20.sol";
 import "@openzeppelin/contracts/token/ERC20/extensions/ERC20Burnable.sol";
 
-contract Ftoken is ERC20 {
+contract Ftoken is ERC20, ERC20Burnable {
     constructor() ERC20("FTOKEN", "FTK") {
         _mint(msg.sender, 100000000000000000000000000000000);
     }
