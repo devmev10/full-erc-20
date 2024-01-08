4f57af521a5fe5b2462e878791c53c3a4be32204
devmev10 <ravitokas90@gmail.com>
2024-01-08 15:50:51 +0530

    add ownable

--- Ftoken.sol
+++ Ftoken.sol
@@ -5,8 +5,12 @@ import "@openzeppelin/contracts/token/ERC20/extensions/ERC20Burnable.sol";
 import "@openzeppelin/contracts/token/ERC20/extensions/ERC20Pausable.sol";
 import "@openzeppelin/contracts/access/Ownable.sol";
 
-contract Ftoken is ERC20, ERC20Burnable {
+contract Ftoken is ERC20, ERC20Burnable, ERC20Pausable, Ownable {
     constructor() ERC20("FTOKEN", "FTK") {
         _mint(msg.sender, 10000000000000 * (10 ** decimals()));
+    }
+
+    function pause() public onlyOwner {
+        _pause();
     }
 }
